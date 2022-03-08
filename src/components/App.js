87c65073d96bf64d47fa36c2e2e58897c3bd3666
Megan Palacios <meg2086@outlook.com>
2022-03-07 21:40:17 -0500

    -some data manipulation that is supposed to work but doesn't, fix tomorrow

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
+import pokemon from '../data/pokemon/pokemon.js';
 // console.log(pokemon);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
@@ -22,5 +22,42 @@ const App = () => {
 
   return el;
 };
+
+function fetchData() {
+  fetch('./data/pokemon/pokemon.json')
+  .then(response => {
+    if (response.ok === false) {
+      throw Error('Ha ocurrido un error');
+    }
+    return response.json();
+  })
+  .then(console.log(response.json()))
+  .then(pokemon => {
+    const cards = pokemon.items
+      .map(cards => {
+        return `
+        <div class='card'>
+          <img src="${items.image}" alt="${cards.id}">
+        </div>
+        `;
+      })
+      .join('');
+    document.querySelector('#allCards').insertAdjacentHTML("afterbegin", cards);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
+/* 	.then(function(data) {
+		console.log(data);
+    allCards = data.items;
+    console.log(allCards);
+    allCards.forEach(items => {
+      Object.entries(items).forEach(([key, value]) => {
+          console.log(`${key} ${value}`);
+      });
+    });
+  }); */
+  
 
 export default App;
