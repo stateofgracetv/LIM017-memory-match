de01843bcf4c4ef6567630027cce38f5475e5684
carmen0810 <carmen.herrera.anicama@gmail.com>
2022-03-10 09:05:32 -0500

    html, css , js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,3 @@ import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
 
-function voltear_cara_sello() {
-    document.getElementById("voltearCara").style.transform = "rotateY(-180deg)";
-    document.getElementById("voltearSello").style.transform = "rotateX(0deg) rotateY(0deg)";
-
-    
-}
-
-document.getElementById("voltearCara").addEventListener("click", voltear_cara_sello);
-
-function voltear_sello_cara() {
-   
-    document.getElementById("voltearCara").style.transform = "rotateX(0deg) rotateY(0deg)";
-    document.getElementById("voltearSello").style.transform = "rotateY(180deg)";
- 
-}
-
-document.getElementById("voltearSello").addEventListener("click", voltear_sello_cara);
