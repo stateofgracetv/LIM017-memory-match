cf63f668db6fc784306b0868df47b89454b26627
carmen0810 <carmen.herrera.anicama@gmail.com>
2022-03-14 11:42:54 -0500

    funciones de girar carta

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 const App = () => {
   const el = document.createElement('div');
-
   el.className = 'App';
   el.textContent = 'Hola mundo!';
 
@@ -9,28 +8,67 @@ const App = () => {
 
 function fetchData() {
   fetch('./data/pokemon/pokemon.json')
-  .then(response => {
-    if (response.ok === false) {
-      throw Error('Ha ocurrido un error');
-    }
-    return response.json();
-  })
-  .then(pokemon => {
-    const cards = pokemon.items
-      .map(cards => {
-        return `
-        <p class="card" id="${cards.id}-1" style="background-color:${cards.bgColor}"><img src="${cards.image}" alt="${cards.id}" /></p>
-        <p class="card" id="${cards.id}-2" style="background-color:${cards.bgColor}"><img src="${cards.image}" alt="${cards.id}" /></p>
+    .then(response => {
+      if (response.ok === false) {
+        throw Error('Ha ocurrido un error');
+      }
+      return response.json();
+    })
+    .then(pokemon => {
+      const cards = pokemon.items
+        .map(cards => {
+          return `
+        <div class="card" style="height:100px">
+        <div class="face hide" id="flip-face_${cards.id}-1" pokemon-back-id="flip-back_${cards.id}-1"><img src="${cards.image}" height="100" alt="${cards.id}" /></div>
+        <div class="back show" id="flip-back_${cards.id}-1" pokemon-face-id="flip-face_${cards.id}-1"><img src="img/card-back.png" height="100" /></div>
+        </div>
+        <br>
         `;
-      })
-      .join('');
-    document.querySelector("#allCards").insertAdjacentHTML("afterbegin", cards);
-  })
-  .catch(error => {
-    console.log(error);
-  })
+        })   //HE CREADO 2 ID PARA TENER UN ID QUE MOSTRAR CARTA Y OTRO QUE OCULTAR
+        .join('');
+      document.querySelector("#allCards").insertAdjacentHTML("afterbegin", cards);
+     
+     
+      function flip_back_face() { //GIRAR SELLO CARA
+
+        document.getElementById(this.getAttribute("id")).classList.add("hide"); //agrega una clase
+        document.getElementById(this.getAttribute("id")).classList.remove("show"); //elimina una clase
+        
+        document.getElementById(this.getAttribute("pokemon-face-id")).classList.add("show"); //agrega una clase
+        document.getElementById(this.getAttribute("pokemon-face-id")).classList.remove("hide"); //elimina una clase
+        
+
+        
+      }
+
+      function flip_face_back() { //GIRAR CARA SELLO
+        
+        document.getElementById(this.getAttribute("pokemon-back-id")).classList.add("show"); //agrega una clase
+        document.getElementById(this.getAttribute("pokemon-back-id")).classList.remove("hide"); //elimina una clase
+
+        document.getElementById(this.getAttribute("id")).classList.add("hide"); //agrega una clase
+        document.getElementById(this.getAttribute("id")).classList.remove("show"); //elimina una clase
+        
+        
+
+      }
+
+     // AL DAR CLICK LLAMO CON CLASE
+      var backClass = document.getElementsByClassName("back");
+      for (let i = 0; i < backClass.length; i++) {
+        backClass[i].addEventListener("click", flip_back_face);
+      }
+
+      var faceClass = document.getElementsByClassName("face");
+      for (let i = 0; i < faceClass.length; i++) {
+        faceClass[i].addEventListener("click", flip_face_back);
+      }
+
+    })
+    .catch(error => {
+      console.log(error);
+    })
 }
 
 fetchData();
-
 export default App;
--- src/main.js
+++ src/main.js
@@ -8,22 +8,7 @@ goToMenu.addEventListener('click', () => {
   document.getElementById('navbar').style.display = 'none';
   document.getElementById('game-bar').style.display = 'grid';
   document.getElementById('menu').style.display = 'grid';
-})
+});
 
-function voltear_cara_sello() {
-  document.getElementById("voltearCara").style.transform = "rotateY(-180deg)";
-  document.getElementById("voltearSello").style.transform = "rotateX(0deg) rotateY(0deg)";
 
-  
-}
 
-document.getElementById("voltearCara").addEventListener("click", voltear_cara_sello);
-
-function voltear_sello_cara() {
- 
-  document.getElementById("voltearCara").style.transform = "rotateX(0deg) rotateY(0deg)";
-  document.getElementById("voltearSello").style.transform = "rotateY(180deg)";
-
-}
-
-document.getElementById("voltearSello").addEventListener("click", voltear_sello_cara);
