fdfbef7beb7f6f1fd979a35698d1b24d6985451e
Megan Palacios <meg2086@outlook.com>
2022-03-23 18:01:22 -0500

    algunos tests

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,6 +1,6 @@
 import fetchData from './App.js';
 import shuffledArray from './ShuffledArray.js';
-const testArray = [{ "id": "bulbasaur", "image": "https://www.serebii.net/pokemongo/pokemon/001.png", "bgColor": "#339933" }, { "id": "ivysaur", "image": "https://www.serebii.net/pokemongo/pokemon/002.png", "bgColor": "#339933" }];
+const testArray = [{ "id": "bulbasaur", "image": "https://www.serebii.net/pokemongo/pokemon/001.png", "bgColor": "#339933" }, { "id": "ivysaur", "image": "https://www.serebii.net/pokemongo/pokemon/002.png", "bgColor": "#339933" }, {"id": "venusaur", "image": "https://www.serebii.net/pokemongo/pokemon/003.png", "bgColor": "#339933"}];
 
 describe('fetchData', () => {
   /* it('should render without crashing', () => {
@@ -22,21 +22,31 @@ describe('fetchData', () => {
     //const expectedElement = shuffledArray(testArray).document.getElementsByTagName("a[href='sampleHref']");
     expect(el.getElementsByTagName("div")).toBeTruthy();
   });
-
-  
-
-  
-
 });
+
+describe('isMatch', () => {
+  it('should return a number', () => {
+    expect(typeof fetchData.play.isMatch()).toBe('number');
+  })
+  it('should return a number greater than zero', () => {
+    expect(fetchData.play.isMatch()).toBeGreaterThan(0);
+  })
+})
 
 describe('shuffledArray', () => {
   it('should be a function', () => {
     expect(typeof shuffledArray).toBe('function');
   });
 
-  it('should return a list', () => {
-    expect(shuffledArray(testArray)).toBe(['something']);
+  it('should be something', () => {
+    expect(shuffledArray(testArray)).toEqual(expect.anything());
   });
 
- 
+  it('should be an array', () => {
+    expect(typeof shuffledArray(testArray)).toBe('array');
+  });
+
+  it('should return a shuffled version of the array', () => {
+    expect(shuffledArray(testArray)).not.toEqual('testArray')
+  })
 });
